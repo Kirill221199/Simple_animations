fffc0140d8558342be608c70b3248bbf2396232f
Кирилл <agarin221199@mail.ru>
2022-06-10 03:42:31 +0500

    Added animation - transitions between fragments

--- app/src/main/java/ru/kirill/simple_animations/MainFragment.kt
+++ app/src/main/java/ru/kirill/simple_animations/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import ru.kirill.simple_animations.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -33,31 +34,31 @@ class MainFragment : Fragment() {
 
     private fun chooseAnimationFragment(){
         binding.fragmentSimpleTransition.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,SimpleTransitionFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,SimpleTransitionFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentExplodeTrensition.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,ExplodeTransitionFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,ExplodeTransitionFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentCropPhoto.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,CropPhotoFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,CropPhotoFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentTrajectoryMove.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,TrajectoryMoveFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,TrajectoryMoveFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentObjectAnimator.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,ObjectAnimatorFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,ObjectAnimatorFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentStateListAnimator.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,StateListAnimatorFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,StateListAnimatorFragment.newInstance()).addToBackStack("").commit()
         }
 
         binding.fragmentConstraintSet.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.container,ConstraintSetFragment.newInstance()).addToBackStack("").commit()
+            parentFragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.container,ConstraintSetFragment.newInstance()).addToBackStack("").commit()
         }
     }
 
